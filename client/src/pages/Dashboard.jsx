6cde891fd2f5b84e949922f78f4d9ba326d2cddb
vivek <vivekkumar16777@gmail.com>
2025-07-30 20:42:38 +0530

    create pages and routes

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>
+        cccc
+    </div>
+  )
+}
+
+export default Dashboard
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      teri ma ki chuk
+    </div>
+  )
+}
+
+export default Home
